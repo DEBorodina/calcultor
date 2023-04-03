5a54da26dcae02c3081236b6c5372b88a336a300
daria_borodina <dasha.borodina2003@mail.ru>
2023-04-03 21:56:37 +0300

    add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Router } from '@/router/Router';
 import Global from '@/styles/global';
 import GlobalThemProvider from '@/components/GlobalThemeProvider';
@@ -10,13 +11,15 @@ const rootElement = document.getElementById('root')!;
 const root = createRoot(rootElement);
 
 root.render(
-  <Provider store={store}>
-    {' '}
-    <GlobalThemProvider>
-      <Global />
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </GlobalThemProvider>
-  </Provider>
+  <ErrorBoundary fallback={<h1>Something went wrong...</h1>}>
+    <Provider store={store}>
+      {' '}
+      <GlobalThemProvider>
+        <Global />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </GlobalThemProvider>
+    </Provider>
+  </ErrorBoundary>
 );
